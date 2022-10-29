1e35dc7e04751f43d161d0e67efe529ea43bba27
Nathan Ifill <nathan.ifill@live.co.uk>
2022-10-29 20:41:12 +0100

    Move config variables to _app:

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,16 @@
 import "../styles/globals.css";
 import "../styles/design_tokens.css";
 
+export const STARTING_NUMBER_OF_POSTS = 7;
+// The maximum number of posts the home page should show before showing the 'Load More' button
+// This includes the post linked in the hero image so for a hero and three blog cards, this should be '4'
+
+export const BLOG_NAME = "The Running Dog Blog";
+// Name of the blog
+
+export const TITLE_SEPARATOR = "|";
+// How the blog name is separated from the page title in the document title, e.g. "|" or "-". Spaces are added automatically
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
--- pages/config.js
+++
@@ -1,9 +0,0 @@
-export const STARTING_NUMBER_OF_POSTS = 7;
-// The maximum number of posts the home page should show before showing the 'Load More' button
-// This includes the post linked in the hero image so for a hero and three blog cards, this should be '4'
-
-export const BLOG_NAME = "The Running Dog Blog";
-// Name of the blog
-
-export const TITLE_SEPARATOR = "|";
-// How the blog name is separated from the page title in the document title, e.g. "|" or "-". Spaces are added automatically
